a1bec7d0bb42d911f51e28172c8e99091b14c7da
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-01-21 16:57:59 +0900

    Update mk_list.py

--- mk_list.py
+++ mk_list.py
@@ -9,88 +9,102 @@ Created on Tue Oct 29 16:53:20 2019
 
 import numpy as np
 import glob, os
-
-
-# ----- Manual setting for standard flat ----- #
-std_flat = 'S20190228S0015'    # GCAL flat for the standard star
-std_arc_w = '700'    # ARC wav0 for the standard star
+import pandas as pd
 
 
 # ----- Directories ----- #
-dir_bias = 'bias'
-dir_std = 'standard'
-dir_red = 'redux6'
+dir_bias = 'bias/'
+dir_std = 'standard/'
+dir_red = 'redux/'
 
-df = np.genfromtxt('info.txt', dtype=None, encoding='ascii',
-	               names=('name','obstype','obsclass','wav0'))
+for di in [dir_bias, dir_std, dir_red]:
+	if (glob.glob(di) == []):
+		os.system('mkdir '+di)
 
-obj = ((df['obstype'] == 'OBJECT') & (df['obsclass'] == 'science'))
-cen_wav, cnt_wav = np.unique(df['wav0'][obj], return_counts=True)
-
-os.system('mkdir '+dir_bias)
-os.system('mkdir '+dir_std)
-for j in np.arange(len(cen_wav)):
-	os.system('mkdir '+dir_red+'_'+str(int(cen_wav[j])))
+current_dir = os.getcwd()
 
 
-# ----- Writing list files ----- #
+# ----- Reading info.txt ----- #
+df = np.genfromtxt('info.txt', dtype=None, encoding='ascii', comments='#',
+	               names=('name','obstype','obsclass','wav0','datalab',
+	               	      'exptime','mask','grating'))
+dlab_id = pd.Series(df['datalab']).str[:-4].values
+seq_num = pd.Series(df['datalab']).str[-3:].values.astype(np.int)
 
-# BIAS
+
+# ----- Data classification ----- #
 bias = (df['obstype'] == 'BIAS')
-os.system('mkdir '+dir_bias)
-f = open(dir_bias+'/'+'bias.lis','w')
+arc = (df['obstype'] == 'ARC')
+flat = (df['obstype'] == 'FLAT')
+obj = (df['obstype'] == 'OBJECT')
+
+
+# ----- OBJECT & FLAT list ----- #
+sci = (obj & (df['obsclass'] == 'science'))
+cen_wav, cnt_wav = np.unique(df['wav0'][sci], return_counts=True)
+for i in np.arange(len(cen_wav)):
+	dir_wav = f"w{10*cen_wav[i]:.0f}/"
+	if (glob.glob(dir_red+dir_wav) == []):
+		os.system('mkdir '+dir_red+dir_wav)
+
+	sci_wav = (sci & (df['wav0'] == cen_wav[i]))
+	sci_idx = np.where(sci_wav)[0]
+
+	f = open(dir_red+dir_wav+'sci.lis','w')
+	g = open(dir_red+dir_wav+'flat.lis','w')
+	for j in np.arange(cnt_wav[i]):
+		f.write(df['name'][sci_wav][j]+'\n')
+		if ((df['obstype'][sci_idx[j]-1] == 'FLAT') & \
+			(dlab_id[sci_idx[j]-1] == dlab_id[sci_idx[j]])):
+			flat_idx = sci_idx[j]-1
+		elif ((df['obstype'][sci_idx[j]+1] == 'FLAT') & \
+			  (dlab_id[sci_idx[j]+1] == dlab_id[sci_idx[j]])):
+			flat_idx = sci_idx[j]+1
+		g.write(df['name'][flat_idx]+'\n')
+	f.close()
+	g.close()
+
+
+# ----- STARNDARD & FLAT list ----- #
+std = (obj & (df['obsclass'] == 'partnerCal'))
+std_idx = np.where(std)[0][0]
+
+if (np.sum(std) != 1):
+	raise ValueError("Please check if the number of standard star frame is one!")
+
+f = open(dir_std+'std.lis','w')
+f.write(df['name'][std_idx]+'\n')
+f.close()
+
+if ((df['obstype'][std_idx-1] == 'FLAT') & \
+	(dlab_id[std_idx-1] == dlab_id[std_idx])):
+	std_flat_idx = std_idx-1
+elif ((df['obstype'][std_idx+1] == 'FLAT') & \
+	  (dlab_id[std_idx+1] == dlab_id[std_idx])):
+	std_flat_idx = std_idx+1
+
+g = open(dir_std+'std_flat.lis','w')
+g.write(df['name'][std_flat_idx]+'\n')
+g.close()
+
+
+# ----- BIAS list ----- #
+f = open(dir_bias+'bias.lis','w')
 for i in np.arange(np.sum(bias)):
 	f.write(df['name'][bias][i]+'\n')
 f.close()
 
-# ARC
-arc = (df['obstype'] == 'ARC')
-cen_wav, cnt_wav = np.unique(df['wav0'][arc], return_counts=True)
 
-for j in np.arange(len(cen_wav)):
-	di = dir_red+'_'+str(int(cen_wav[j]))
-	f = open(di+'/'+'arc.lis','w')
-	arc_w = (df['wav0'][arc] == cen_wav[j])
-	for i in np.arange(cnt_wav[j]):
-		f.write(df['name'][arc][arc_w][i]+'\n')
+# ----- ARC list ----- #
+for i in np.arange(np.sum(arc)):
+	arc_wav0 = df['wav0'][arc][i]
+	dir_wav = f"w{10*arc_wav0:.0f}/"
+	f = open(dir_red+dir_wav+'arc.lis','w')
+	f.write(df['name'][arc][i]+'\n')
 	f.close()
-	if (cen_wav[j] == float(std_arc_w)):
-		os.system('cp -rpv '+di+'/'+'arc.lis '+dir_std+'/'+'std_arc.lis')
 
-# FLAT
-flat = ((df['obstype'] == 'FLAT') & (df['obsclass'] == 'partnerCal'))
-cen_wav, cnt_wav = np.unique(df['wav0'][flat], return_counts=True)
-
-for j in np.arange(len(cen_wav)):
-	di = dir_red+'_'+str(int(cen_wav[j]))
-	f = open(di+'/'+'flat.lis','w')
-	flat_w = (df['wav0'][flat] == cen_wav[j])
-	for i in np.arange(cnt_wav[j]):
-		if (df['name'][flat][flat_w][i] != std_flat):
-			f.write(df['name'][flat][flat_w][i]+'\n')
-	f.close()	
-
-f = open(dir_std+'/'+'std_flat.lis','w')
-f.write(std_flat+'\n')
-f.close()
-
-# STANDARD
-std = ((df['obstype'] == 'OBJECT') & (df['obsclass'] == 'partnerCal'))
-f = open(dir_std+'/'+'std.lis','w')
-for i in np.arange(np.sum(std)):
-	f.write(df['name'][std][i]+'\n')
-f.close()
-
-# OBJECT
-obj = ((df['obstype'] == 'OBJECT') & (df['obsclass'] == 'science'))
-cen_wav, cnt_wav = np.unique(df['wav0'][obj], return_counts=True)
-
-for j in np.arange(len(cen_wav)):
-	di = dir_red+'_'+str(int(cen_wav[j]))
-	os.system('mkdir '+di)
-	f = open(di+'/'+'sci.lis','w')
-	obj_w = (df['wav0'][obj] == cen_wav[j])
-	for i in np.arange(cnt_wav[j]):
-		f.write(df['name'][obj][obj_w][i]+'\n')
-	f.close()
+	if (arc_wav0 == df['wav0'][std_idx]):
+		g = open(dir_std+'std_arc.lis','w')
+		g.write(df['name'][arc][i]+'\n')
+		g.close()
 
