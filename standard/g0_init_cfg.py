6663105c3df1c90b5ebfb0471d2352ee05a9f02f
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-03-25 11:19:08 +0900

    Update g0_init_cfg.py

--- standard/g0_init_cfg.py
+++ standard/g0_init_cfg.py
@@ -11,7 +11,7 @@ import numpy as np
 import glob, os
 
 
-# ----- File name & directory ----- #
+# ----- Directory & file name & basic setting ----- #
 procbias = 'Mbias.fits'
 rawdir = '../raw/'
 caldir = '../calibrations/'
@@ -22,18 +22,61 @@ lst_std = 'std.lis'
 lst_arc = 'std_arc.lis'
 lst_flat = 'std_flat.lis'
 
+# Slit mode of IFU
 nslit = 2
 
 
-# Find star w/ iraf.dir('onedstds') or iraf.dir('onedstds$oke1990')
+# ----- Find star w/ iraf.dir('onedstds') ----- #
+## Absolute path: ~/[anaconda home directory]/envs/iraf27/iraf/noao/lib/onedstds/
+## Check the standard star name from the header of the raw image
+## $ find . -name *[standard starname]*
+
+
 starname = 'gd108'
+'''
+Standard star name
+exact name of [starname].dat
+'''
+
 stardir = 'onedstds$oke1990/'
+'''
+Directory name of standard star data file
+'onedstds$[subdirectory]'
+'''
+
 extinction = 'onedstds$ctioextinct.dat'
+'''
+Extinction file name
+GMOS-N: 'mk_extinct.txt' (needed to be downloaded)
+GMOS-S: 'onedstds$ctioextinct.dat'
+'''
+
 root_name = starname+'_700_20190304_'
+'''
+Output file name for sensitivity function
+(i.e. [starname]_[centwave]_[obsdate]_)
+'''
+
 obs_site = 'Gemini-South'
+'''
+Observing site
+'Gemini-North' or 'Gemini-South'
+'''
 
-# Check the mdf name w/ iraf.dir('gmos$data/*ifu*.fits', ncols=1)
+
+# ----- Check the mdf name w/ iraf.dir('gmos$data/*ifu*.fits', ncols=1) ----- #
+## Absolute path: ~/[anaconda home directory]/envs/geminiconda/iraf_extern/gemini/gmos/data/
 mdf = 'gsifu_slits_mdf_HAM.fits'
-nmdf = 'new_gsifu_slits_mdf_HAM.fits'    
-bias = '../calibrations/Mbias.fits'
+'''
+Default MDF name
+g[n/s]ifu_[ns]_slit[b/r/s]_mdf_[CCD].fits
+n/s: Gemini north or south
+[ns]: Nod & shuffle mode or not
+[b/r/s]: blue/red/two slit mode
+[CCD]: EEV or HAMAMATSU
+'''
 
+nmdf = 'new_gsifu_slits_mdf_HAM.fits'
+'''
+New MDF name
+'''
