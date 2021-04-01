fa67302bd357b2134f261c64bd86c4180e64d0e0
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-04-01 16:12:16 +0900

    Update g2_trace_ref.py

--- standard/g2_trace_ref.py
+++ standard/g2_trace_ref.py
@@ -15,6 +15,15 @@ import glob, os
 import g0_init_cfg as ic
 
 
+# ----- Line number (to be revised!) ----- #
+pk_line = 1400
+'''
+Line for finding peaks (gfreduce)
+Line/column for finding apertures (gfextract)
+'''
+# ---------------------------------------- #
+
+
 # ----- Importing IRAF from the root directory ----- #
 current_dir = os.getcwd()
 os.chdir(ic.dir_iraf)
@@ -30,22 +39,29 @@ iraf.unlearn('gfdisplay')
 
 
 # ---------- Trace reference ---------- #
+flat = np.loadtxt(ic.lst_flat, dtype=str)
+if (flat.size > 1):
+	raise ValueError("Please check if there is only one flat image for the standard star.")
+flat0 = flat.item(0)
+
 iraf.imdelete('g@'+ic.lst_flat)
 iraf.imdelete('rg@'+ic.lst_flat)
 iraf.imdelete('erg@'+ic.lst_flat)
 
-for flat in iraf.type(ic.lst_flat, Stdout=1):
-	flat = flat.strip()
-	iraf.gfreduce(flat, rawpath=ic.rawdir, fl_extract='yes', bias=ic.bias,
-                  fl_over='yes', fl_trim='yes', mdffile=ic.nmdf, mdfdir='./',
-                  slits='both', line=1400, fl_fluxcal='no', fl_gscrrej='no',
-                  fl_wavtran='no', fl_skysub='no', fl_inter='no', fl_vardq='yes')
+iraf.gfreduce(flat0, rawpath=ic.rawdir, fl_extract='yes', bias=ic.caldir+ic.procbias,
+	          fl_over='yes', fl_trim='yes', mdffile=ic.nmdf, mdfdir='./',
+	          slits=ic.cslit, line=pk_line, fl_fluxcal='no', fl_gscrrej='no',
+	          fl_wavtran='no', fl_skysub='no', fl_inter='no', fl_vardq='yes')
 
+
+# ----- Displaying the results ----- #
+if (ic.nslit == 1):
+	vkw = '1'
+if (ic.nslit == 2):
+	vkw = '*'
 os.system('ds9 &')
 iraf.sleep(5.0)
-for flat in iraf.type(ic.lst_flat, Stdout=1):
-    flat = flat.strip()
-    iraf.gfdisplay('erg'+flat, 1)
+iraf.gfdisplay('erg'+flat, 1, version=vkw)
 
 
 # Printing the running time
