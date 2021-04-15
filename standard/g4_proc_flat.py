1c7742b1b0d8ce5e3ed4baf9270687c018cf0fcf
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-04-15 10:56:43 +0900

    Update g4_proc_flat.py

--- standard/g4_proc_flat.py
+++ standard/g4_proc_flat.py
@@ -15,6 +15,15 @@ import glob, os
 import g0_init_cfg as ic
 
 
+# ----- Line number (to be revised!) ----- #
+pk_line = 1400
+'''
+Line for finding peaks (gfreduce)
+Line/column for finding apertures (gfextract)
+'''
+# ---------------------------------------- #
+
+
 # ----- Importing IRAF from the root directory ----- #
 current_dir = os.getcwd()
 os.chdir(ic.dir_iraf)
@@ -32,20 +41,27 @@ iraf.unlearn('gfresponse')
 
 
 # ---------- Reduce the lamp flat ---------- #
+flat = np.loadtxt(ic.lst_flat, dtype=str)
+if (flat.size > 1):
+    raise ValueError("Please check if there is only one flat image for the standard star.")
+flat0 = flat.item(0)
+
+arc = np.loadtxt(ic.lst_arc, dtype=str)
+if (arc.size > 1):
+    raise ValueError("Please check if there is only one arc image for the standard star.")
+arc0 = arc.item(0)
 
 # Model and remove the light
-
 iraf.imdelete('brg@'+ic.lst_flat)
 
 # os.system('ds9 &')
 # iraf.sleep(5.0)
-for flat in iraf.type(ic.lst_flat, Stdout=1):
-    flat = flat.strip()
-    blkmsk = 'newblkmask_S20190228S0015_hdr01'
-    iraf.gfscatsub('rg'+flat, blkmsk, outimage='', prefix='b',
-    	             xorder='3,3,3,3,3,3,3,3,3,3,3,3',
-    	             yorder='3,3,3,3,3,3,3,3,3,3,3,3', 
-                   cross='yes', fl_inter='no')
+blkmsk = np.loadtxt("blkmask_name.txt", dtype=str).item(0)
+blkmsk0 = blkmsk
+iraf.gfscatsub('rg'+flat0, blkmsk0, outimage='', prefix='b',
+               xorder='3,3,3,3,3,3,3,3,3,3,3,3',
+               yorder='3,3,3,3,3,3,3,3,3,3,3,3', 
+               cross='yes', fl_inter='no')
 
 # os.system('ds9 &')
 # iraf.sleep(5.0)
@@ -56,41 +72,35 @@ for flat in iraf.type(ic.lst_flat, Stdout=1):
 
 
 # QE correction and extract
-iraf.imdelete('eqbrg@'+ic.lst_flat)
-
-flatref = iraf.type(ic.lst_flat, Stdout=1)[0]
-arc0 = iraf.type(ic.lst_arc, Stdout=1)[0]
-
 iraf.imdelete('qbrg@'+ic.lst_flat)
 iraf.imdelete('eqbrg@'+ic.lst_flat)
-iraf.gfreduce('brg@'+ic.lst_flat, recenter='no', reference='erg'+flatref,
+iraf.gfreduce('brg@'+ic.lst_flat, recenter='no', reference='erg'+flat0,
               fl_extract='yes', fl_qecorr='yes', qe_refim='erg'+arc0,
               fl_addmdf='no', fl_bias='no', fl_over='no', fl_trim='no',
               mdffile=ic.nmdf, mdfdir='./',
-              slits='both', line=1400, fl_fluxcal='no', fl_gscrrej='no',
-              fl_wavtran='no', fl_skysub='no', fl_inter='no',
-              fl_vardq='yes')
+              slits=ic.cslit, line=pk_line, fl_fluxcal='no', fl_gscrrej='no',
+              fl_wavtran='no', fl_skysub='no', fl_inter='no', fl_vardq='yes')
 
+if (ic.nslit == 1):
+    vkw = '1'
+if (ic.nslit == 2):
+    vkw = '*'
 os.system('ds9 &')
 iraf.sleep(5.0)
-for flat in iraf.type(ic.lst_flat, Stdout=1):
-	flat = flat.strip()
-	iraf.gfdisplay('eqbrg'+flat, 1)
+iraf.gfdisplay('eqbrg'+flat0, 1, version=vkw)
 
 
 # ---------- Response function ---------- #
 for flat in iraf.type(ic.lst_flat, Stdout=1):
     flat = flat.strip()
-    iraf.imdelete(flat+'_resp')
-    iraf.gfresponse('eqbrg'+flat, outimage=flat+'_resp', sky='', 
-    	              order=45, func='spline3', sample='*', 
-    	              fl_fit='yes', fl_inter='no')
+    iraf.imdelete(flat0+'_resp')
+    iraf.gfresponse('eqbrg'+flat0, outimage=flat0+'_resp', sky='', 
+    	            order=45, func='spline3', sample='*', 
+    	            fl_fit='yes', fl_inter='no')
 
 # os.system('ds9 &')
 # iraf.sleep(5.0)
-for flat in iraf.type(ic.lst_flat, Stdout=1):
-    flat = flat.strip()
-    iraf.gfdisplay(flat+'_resp', 1)
+iraf.gfdisplay(flat0+'_resp', 1, version=vkw)
 
 
 # Printing the running time
