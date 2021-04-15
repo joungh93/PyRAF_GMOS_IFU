0b711c7e7d4022e27138c2e0e003b2d0ca329474
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-04-15 16:23:50 +0900

    Update g7_sci_extract.py

--- standard/g7_sci_extract.py
+++ standard/g7_sci_extract.py
@@ -15,6 +15,15 @@ import glob, os
 import g0_init_cfg as ic
 
 
+# ----- Line number (to be revised!) ----- #
+pk_line = 1400
+'''
+Line for finding peaks (gfreduce)
+Line/column for finding apertures (gfextract)
+'''
+# ---------------------------------------- #
+
+
 # ----- Importing IRAF from the root directory ----- #
 current_dir = os.getcwd()
 os.chdir(ic.dir_iraf)
@@ -31,28 +40,39 @@ iraf.unlearn('gfdisplay')
 
 
 # ---------- QE correction and extraction of the science ---------- #
-flat0 = iraf.type(ic.lst_flat, Stdout=1)[0].strip()
+flat = np.loadtxt(ic.lst_flat, dtype=str)
+if (flat.size > 1):
+    raise ValueError("Please check if there is only one flat image for the standard star.")
+flat0 = flat.item(0)
+
+arc = np.loadtxt(ic.lst_arc, dtype=str)
+if (arc.size > 1):
+    raise ValueError("Please check if there is only one arc image for the standard star.")
+arc0 = arc.item(0)
+
+std = np.loadtxt(ic.lst_std, dtype=str)
+if (std.size > 1):
+    raise ValueError("Please check if there is only one image for the standard star.")
+std0 = std.item(0)
+
 response = flat0+'_resp'
 ref_flat0 = 'eqbrg'+flat0
 
-arc0 = iraf.type(ic.lst_arc, Stdout=1)[0].strip()
-
 iraf.imdelete('qxbrg@'+ic.lst_std)
 iraf.imdelete('eqxbrg@'+ic.lst_std)
+iraf.gqecorr('xbrg'+std0, refimage='erg'+arc0, fl_correct='yes',
+             fl_vardq='yes', verbose='yes')
+iraf.gfextract('qxbrg'+std0, response=response, recenter='no',
+               trace='no', reference=ref_flat0, weights='none',
+               fl_vardq='yes', line=pk_line, exslits=ic.eslit)
 
-for std in iraf.type(ic.lst_std, Stdout=1):
-    std = std.strip()
-    iraf.gqecorr('xbrg'+std, refimage='erg'+arc0, fl_correct='yes',
-                 fl_vardq='yes', verbose='yes')
-    iraf.gfextract('qxbrg'+std, response=response, recenter='no',
-                   trace='no', reference=ref_flat0, weights='none',
-                   fl_vardq='yes', line=1400)
-
+if (ic.nslit == 1):
+    vkw = '1'
+if (ic.nslit == 2):
+    vkw = '*'
 os.system('ds9 &')
 iraf.sleep(5.0)
-for std in iraf.type(ic.lst_std, Stdout=1):
-	std = std.strip()
-	iraf.gfdisplay('eqxbrg'+std, 1)
+iraf.gfdisplay('eqxbrg'+std0, 1, version=vkw)
 
 
 # Printing the running time
