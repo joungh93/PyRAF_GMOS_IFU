19de99225f1bc01f12ac4c40f3e777bd73ba0f3f
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-04-28 17:58:20 +0900

    Update g8_fixcol.py

--- standard/g8_fixcol.py
+++ standard/g8_fixcol.py
@@ -11,8 +11,10 @@ import time
 start_time = time.time()
 
 import numpy as np
-import glob, os
+import glob, os, copy
 import g0_init_cfg as ic
+from astropy.io import fits
+from reg_saoimage import read_region
 
 
 # ----- Importing IRAF from the root directory ----- #
@@ -27,49 +29,38 @@ iraf.chdir(current_dir)
 
 
 # ---------- Fixing a bad column ---------- #
-from astropy.io import fits
-
 std = np.loadtxt(ic.lst_std, dtype=str)
 if (std.size > 1):
     raise ValueError("Please check if there is only one image for the standard star.")
 std0 = std.item(0)
 
-fits.open('eqxbrg'+std0+'.fits').info()
 # 2-slit mode
-dt1, hd1 = fits.getdata('eqxbrg'+std+'.fits', ext=2, header=True)
+dt1, hd1 = fits.getdata('eqxbrg'+std0+'.fits', ext=2, header=True)
 if (ic.nslit == 2):
-	dt2, hd2 = fits.getdata('eqxbrg'+std+'.fits', ext=5, header=True)
+	dt2, hd2 = fits.getdata('eqxbrg'+std0+'.fits', ext=5, header=True)
 
-os.system('ds9 &')
-iraf.sleep(5.0)
-iraf.display(image = 'eqxbrg'+std+'.fits[sci,1]', frame=1)
+
+# ----- Reading masking coordinates ----- #
+if (ic.nslit == 1):
+    fix_std = {'slit1': [[], []], 'slit2': [[], []]}
 if (ic.nslit == 2):
-	iraf.display(image = 'eqxbrg'+std+'.fits[sci,2]', frame=2)
+    fix_std = {'slit1': [[], []], 'slit2': [[], []]}
 
-
-# # Stop point #1 : please check the 'physical' coordinates in the images!
-# import sys
-# sys.exit('Image check ...')
-
-
-# Mask coordinates
-import copy
-
-
-# ----- Format ----- #
-#fix_sci{n} = {'slit1' : [[centers], [widths]],
-#              'slit2' : [[centers], [widths]]}
-
-fix_sci01 = {'slit1' : [[2352, 1484], [16,10]],
-             'slit2' : [[496, 2310, 1748], [6, 10, 20]]}
-# ------------------ #
+for i in 1+np.arange(ic.nslit):
+    regfile = "badcol/std1_slit{0:1d}.reg".format(i)
+    if (glob.glob(regfile) == []):
+        continue
+    else:
+        x0, y0, xs, ys, _ = read_region(regfile, regtype="box")
+        fix_std["slit{0:1d}".format(i)][0].append(int(round(x0[0]))-1)
+        fix_std["slit{0:1d}".format(i)][1].append(int(round(xs[0]))-1)
 
 
 for i in np.arange(len(iraf.type(ic.lst_std, Stdout=1))):
     std = iraf.type(ic.lst_std, Stdout=1)[i].strip()
     exec('dic = copy.deepcopy(fix_sci{0:02d})'.format(i+1))
     for j in np.arange(ic.nslit):
-    	fmsk = 'mskbadcol_'+std+'_{0:1d}.txt'.format(j+1)
+    	fmsk = 'mskbadcol_'+std0+'_{0:1d}.txt'.format(j+1)
     	s = dic['slit{0:1d}'.format(j+1)]
     	exec('d = dt{0:1d}'.format(j+1))
     	for k in np.arange(len(s[0])):
@@ -87,20 +78,20 @@ iraf.imdelete('tmpdq@'+ic.lst_std)
 
 for std in iraf.type(ic.lst_std, Stdout=1):
     std = std.strip()
-    iraf.copy('eqxbrg'+std+'.fits', 'tmp'+std+'.fits')
+    iraf.copy('eqxbrg'+std0+'.fits', 'tmp'+std0+'.fits')
     for j in np.arange(ic.nslit):
-    	iraf.proto.fixpix('tmp'+std+'.fits[sci,{0:1d}]'.format(j+1),
-    		              'mskbadcol_'+std+'_{0:1d}.pl'.format(j+1),
+    	iraf.proto.fixpix('tmp'+std0+'.fits[sci,{0:1d}]'.format(j+1),
+    		              'mskbadcol_'+std0+'_{0:1d}.pl'.format(j+1),
     		              linterp='1,2,3,4')
-    iraf.copy('tmp'+std+'.fits', 'xeqxbrg'+std+'.fits')
+    iraf.copy('tmp'+std0+'.fits', 'xeqxbrg'+std0+'.fits')
     for j in np.arange(ic.nslit):
-    	iraf.imarith('mskbadcol_'+std+'_{0:1d}.pl'.format(j+1), '+',
-    		         'xeqxbrg'+std+'.fits[dq,{0:1d}]'.format(j+1),
-    		         'tmpdq'+std+'.fits[dq,{0:1d}]'.format(j+1))
-    	iraf.imcopy('tmpdq'+std+'.fits[dq,{0:1d}][*,*]'.format(j+1),
-    		        'xeqxbrg'+std+'.fits[dq,{0:1d}][*,*]'.format(j+1))
-    	iraf.proto.fixpix('tmp'+std+'.fits[sci,{0:1d}]'.format(j+1),
-    		              'mskbadcol_'+std+'_{0:1d}.pl'.format(j+1),
+    	iraf.imarith('mskbadcol_'+std0+'_{0:1d}.pl'.format(j+1), '+',
+    		         'xeqxbrg'+std0+'.fits[dq,{0:1d}]'.format(j+1),
+    		         'tmpdq'+std0+'.fits[dq,{0:1d}]'.format(j+1))
+    	iraf.imcopy('tmpdq'+std0+'.fits[dq,{0:1d}][*,*]'.format(j+1),
+    		        'xeqxbrg'+std0+'.fits[dq,{0:1d}][*,*]'.format(j+1))
+    	iraf.proto.fixpix('tmp'+std0+'.fits[sci,{0:1d}]'.format(j+1),
+    		              'mskbadcol_'+std0+'_{0:1d}.pl'.format(j+1),
     		              linterp='1,2,3,4')    
 
 
@@ -108,8 +99,8 @@ for std in iraf.type(ic.lst_std, Stdout=1):
 	for j in np.arange(ic.nslit):
 		os.system('ds9 &')
 		iraf.sleep(5.0)
-		iraf.display(image = 'eqxbrg'+std+'.fits[sci,{0:1d}]'.format(j+1), frame=1)
-		iraf.display(image = 'xeqxbrg'+std+'.fits[sci,{0:1d}]'.format(j+1), frame = 2)
+		iraf.display(image = 'eqxbrg'+std0+'.fits[sci,{0:1d}]'.format(j+1), frame=1)
+		iraf.display(image = 'xeqxbrg'+std0+'.fits[sci,{0:1d}]'.format(j+1), frame = 2)
 
 
 # Printing the running time
