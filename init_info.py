e81c7a6dbfd1450566190f770a399f3a42d41865
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-08-17 17:38:07 +0900

    Update init_info.py

--- init_info.py
+++ init_info.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Oct 29 16:53:20 2019
-
 @author: jlee
 """
 
@@ -23,26 +22,30 @@ rawfile = sorted(glob.glob('*.fits'))
 
 # ----- Reading FITS headers ----- #
 f = open(current_dir+'/'+'info.txt','w')
+f.write("# FILENAME  OBJTYPE  OBSCLASS  CENTWAVE  DATALAB  MASKNAME  GRATING  AIRMASS  MJD-OBS\n")
 for i in rawfile:
-	hdr = fits.getheader(i, ext=0)
-	objtype = hdr['OBSTYPE'].strip()
-	objclass = hdr['OBSCLASS'].strip()
-	centwave = str(hdr['CENTWAVE'])
-	datalabel = hdr['DATALAB'].strip()
-	exptime = f"{hdr['EXPTIME']:.1f}"
-	mask = hdr['MASKNAME'].strip()
-	grating = hdr['GRATING'].strip()
+	h0 = fits.getheader(i, ext=0)
+	h1 = fits.getheader(i, ext=1)
+	objtype = h0['OBSTYPE'].strip()
+	objclass = h0['OBSCLASS'].strip()
+	centwave = str(h0['CENTWAVE'])
+	datalabel = h0['DATALAB'].strip()
+	exptime = f"{h0['EXPTIME']:.1f}"
+	mask = h0['MASKNAME'].strip()
+	grating = h0['GRATING'].strip()
+	airmass = f"{h0['AIRMASS']:.4f}"
+	mjd = f"{h1['MJD-OBS']:f}"
 
-	f.write(i.strip('.fits')+'   ')
-	f.write(objtype+'   ')
-	f.write(objclass+'   ')
-	f.write(centwave+'   ')
-	f.write(datalabel+'   ')
-	f.write(exptime+'   ')
-	f.write(mask+'   ')
-	f.write(grating+'\n')
+	f.write(i.strip('.fits')+'  ')
+	f.write(objtype+'  ')
+	f.write(objclass+'  ')
+	f.write(centwave+'  ')
+	f.write(datalabel+'  ')
+	f.write(exptime+'  ')
+	f.write(mask+'  ')
+	f.write(grating+'  ')
+	f.write(airmass+'  ')
+	f.write(mjd+'\n')
 f.close()
 
 os.chdir(current_dir)
-
-
