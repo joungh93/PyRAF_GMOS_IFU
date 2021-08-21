a2d72d6052a31cedf30ca9aa950ae441ad747843
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-08-21 23:05:16 +0900

    Update g0_init_cfg.py

--- redux/g0_init_cfg.py
+++ redux/g0_init_cfg.py
@@ -11,25 +11,96 @@ import numpy as np
 import glob, os
 
 
-# ----- File name & directory ----- #
+# ----- Directory & file name & basic setting ----- #
+cpath = os.path.abspath(".")
+dir_iraf = "/".join(cpath.split("/")[:-1])+"/"
+rawdir = dir_iraf+"raw/"
+caldir = dir_iraf+"calibrations/"
+dir_db = "database/"    # Relative path
+dir_std = dir_iraf+"standard/"
+
 procbias = 'Mbias.fits'
-rawdir = '../Raw/'
-caldir = '../calibrations/'
-dir_iraf = '../'
-dir_db = 'database/'
 
 lst_sci = 'sci.lis'
 lst_arc = 'arc.lis'
 lst_flat = 'flat.lis'
+pk_line = 1400    # Deteremined from standard star reduction
+'''
+Line for finding peaks (gfreduce)
+Line/column for finding apertures (gfextract)
+'''
 
+# Slit mode of IFU
 nslit = 2
+cslit = 'both'
+'''
+Slit mode for IFU (as an input parameter of gfreduce)
+IFU-1 slit: cslit = 'red' / 'blue'
+IFU-2 slit: cslit = 'both'
+'''
 
-# For standard calibration
-sensfunc = 'gd108_700_20190304_sens'
-extinction = 'onedstds$ctioextinct.dat'
-obs_site = 'Gemini-South'
+if (nslit == 1):
+	eslit = cslit
+if (nslit == 2):
+	eslit = '*'
+'''
+Slit mode for IFU (as an input parameter of gfextract)
+IFU-1 slit: eslit = 'red' / 'blue'
+IFU-2 slit: eslit = '*'
+'''
 
-# Check the mdf name w/ iraf.dir('gmos$data/*ifu*.fits', ncols=1)
-mdf = 'gsifu_slits_mdf_HAM.fits'  
-nmdf = 'new_gsifu_slits_mdf_HAM.fits'  
-bias = '../calibrations/Mbias.fits'
+
+# ----- Find star w/ iraf.dir('onedstds') ----- #
+## Absolute path: ~/[anaconda home directory]/envs/iraf27/iraf/noao/lib/onedstds/
+## Check the standard star name from the header of the raw image (extension: 0, keyword: 'OBJECT')
+## $ find . -name *[standard starname]*
+
+
+starname = 'feige34'
+'''
+Standard star name
+exact name of [starname].dat
+'''
+
+stardir = 'onedstds$spec50cal/'
+'''
+Directory name of standard star data file
+'onedstds$[subdirectory]/'
+'''
+
+extinction = dir_iraf+'mk_extinct.txt'
+'''
+Extinction file name
+GMOS-N: 'mk_extinct.txt' (needed to be downloaded from Buton+13)
+GMOS-S: 'onedstds$ctioextinct.dat'
+'''
+
+root_name = starname+'_650_20210408_'
+'''
+Output file name for sensitivity function
+(i.e. [starname]_[centwave]_[obsdate]_)
+'''
+
+obs_site = 'Gemini-North'
+'''
+Observing site
+'Gemini-North' or 'Gemini-South'
+'''
+
+
+# ----- Check the mdf name w/ iraf.dir('gmos$data/*ifu*.fits', ncols=1) ----- #
+## Absolute path: ~/[anaconda home directory]/envs/iraf27/iraf_extern/gemini/gmos/data/
+mdf = 'gnifu_slits_mdf.fits'
+'''
+Default MDF name
+g[n/s]ifu_[ns]_slit[b/r/s]_mdf_[CCD].fits
+n/s: Gemini north or south
+[ns]: Nod & shuffle mode or not
+[b/r/s]: blue/red/two slit mode
+[CCD]: EEV or HAMAMATSU
+'''
+
+nmdf = 'new_'+mdf
+'''
+New MDF name
+'''
