668902fa1aad73dc66a9e68df9dec63e310ff53a
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-08-24 19:40:41 +0900

    Update g8_fixcol.py

--- redux/g8_fixcol.py
+++ redux/g8_fixcol.py
@@ -11,8 +11,10 @@ import time
 start_time = time.time()
 
 import numpy as np
-import glob, os
+import glob, os, copy
 import g0_init_cfg as ic
+from astropy.io import fits
+from reg_saoimage import read_region
 
 
 # ----- Importing IRAF from the root directory ----- #
@@ -27,103 +29,102 @@ iraf.chdir(current_dir)
 
 
 # ---------- Fixing a bad column ---------- #
-from astropy.io import fits
+for d in ic.dir_wav:
+    dir_sci = sorted(glob.glob(d+"/*"))
 
-for sci in iraf.type(ic.lst_sci, Stdout=1):
-    sci = sci.strip()
-    fits.open('eqxbrg'+sci+'.fits').info()
-    # 2-slit mode
-    dt1, hd1 = fits.getdata('eqxbrg'+sci+'.fits', ext=2, header=True)
-    dt2, hd2 = fits.getdata('eqxbrg'+sci+'.fits', ext=5, header=True)
+    for j in np.arange(len(dir_sci)):
 
-    # os.system('ds9 &')
-    # iraf.sleep(5.0)
-    # iraf.display(image = 'eqxbrg'+sci+'.fits[sci,1]', frame = 1)
-    # iraf.display(image = 'eqxbrg'+sci+'.fits[sci,2]', frame = 2)
+        # Moving each science directory
+        name_sci = dir_sci[j].split("/")[-1]
+        print("Moving path for "+name_sci+"...")
+        os.chdir(current_dir+"/"+dir_sci[j])
+        iraf.chdir(current_dir+"/"+dir_sci[j])
 
+        # SCI
+        sci = np.loadtxt(ic.lst_sci, dtype=str)
+        sci0 = sci.item(0)
 
-# # Stop point #1 : please check the 'physical' coordinates in the images!
-# import sys
-# sys.exit('Image check ...')
+        # 2-slit mode
+        dt1, hd1 = fits.getdata('eqxbrg'+sci0+'.fits', ext=2, header=True)
+        if (ic.nslit == 2):
+            dt2, hd2 = fits.getdata('eqxbrg'+sci0+'.fits', ext=5, header=True)
 
+        # Reading masking coordinates
+        if (ic.nslit == 1):
+            fix_sci = {'slit1': [[], []]}
+        if (ic.nslit == 2):
+            fix_sci = {'slit1': [[], []], 'slit2': [[], []]}
 
-# Mask coordinates
-import copy
+        for i in 1+np.arange(ic.nslit):
+            regfile = "badcol/sci1_slit{0:1d}.reg".format(i)
+            if (glob.glob(regfile) == []):
+                fix_sci["slit{0:1d}".format(i)][0].append(100)
+                fix_sci["slit{0:1d}".format(i)][1].append(0)
+            else:
+                x0, y0, xs, ys, _ = read_region(regfile, regtype="box")
+                for j in np.arange(len(x0)):
+                    fix_sci["slit{0:1d}".format(i)][0].append(int(round(x0[j])))
+                    fix_sci["slit{0:1d}".format(i)][1].append(int(round(xs[j])))
 
+        # Saving masking coordinates in text files
+        dic = copy.deepcopy(fix_sci)
+        for j in np.arange(ic.nslit):
+            fmsk = 'mskbadcol_'+sci0+'_{0:1d}.txt'.format(j+1)
+            s = dic['slit{0:1d}'.format(j+1)]
+            exec('d = dt{0:1d}'.format(j+1))
+            for k in np.arange(len(s[0])):
+                if (k == 0):
+                    com = "echo '{0:d} {1:d} 1 {2:d}' > ".format(s[0][k]-s[1][k]/2, s[0][k]+s[1][k]/2, d.shape[0])+fmsk
+                    os.system(com)
+                else:
+                    com = "echo '{0:d} {1:d} 1 {2:d}' >> ".format(s[0][k]-s[1][k]/2, s[0][k]+s[1][k]/2, d.shape[0])+fmsk
+                    os.system(com)
+            iraf.text2mask(fmsk, fmsk.strip('.txt')+'.pl', d.shape[1], d.shape[0])
 
-# ----- Format ----- #
-#fix_sci{n} = {'slit1' : [[centers], [widths]],
-#              'slit2' : [[centers], [widths]]}
+        iraf.imdelete('xeqxbrg@'+ic.lst_sci)
+        iraf.imdelete('tmp@'+ic.lst_sci)
+        iraf.imdelete('tmpdq@'+ic.lst_sci)
 
-# # 700 nm
-# fix_sci01 = {'slit1' : [[2355, 1483, 1783, 1815],
-#                         [28, 14, 26, 26, 24, 26]],
-#              'slit2' : [[1750, 2310, 1800, 1837],
-#                         [30, 13, 26, 26]]}
-# fix_sci02 = {'slit1' : [[1482, 2355, 1783, 1815],
-#                         [12, 28, 26, 26, 24, 26]],
-#              'slit2' : [[1641, 1747, 2310, 1800, 1837],
-#                         [50, 40, 14, 26, 26]]}
+        # Masking bad columns with IRAF/fixpix
+        iraf.copy('eqxbrg'+sci0+'.fits', 'tmp'+sci0+'.fits')
+        for j in np.arange(ic.nslit):
+            iraf.proto.fixpix('tmp'+sci0+'.fits[sci,{0:1d}]'.format(j+1),
+                              'mskbadcol_'+sci0+'_{0:1d}.pl'.format(j+1),
+                              linterp='1,2,3,4')
+        iraf.copy('tmp'+sci0+'.fits', 'xeqxbrg'+sci0+'.fits')
+        for j in np.arange(ic.nslit):
+            iraf.imarith('mskbadcol_'+sci0+'_{0:1d}.pl'.format(j+1), '+',
+                         'xeqxbrg'+sci0+'.fits[dq,{0:1d}]'.format(j+1),
+                         'tmpdq'+sci0+'.fits[dq,{0:1d}]'.format(j+1))
+            iraf.imcopy('tmpdq'+sci0+'.fits[dq,{0:1d}][*,*]'.format(j+1),
+                        'xeqxbrg'+sci0+'.fits[dq,{0:1d}][*,*]'.format(j+1))
+            iraf.proto.fixpix('tmp'+sci0+'.fits[sci,{0:1d}]'.format(j+1),
+                              'mskbadcol_'+sci0+'_{0:1d}.pl'.format(j+1),
+                              linterp='1,2,3,4') 
 
-# 680 nm
-fix_sci01 = {'slit1' : [[1483, 2355],
-                        [12, 28]],
-             'slit2' : [[1632, 1740, 2305],
-                        [40, 50, 14]]}
-fix_sci02 = {'slit1' : [[1484, 2355],
-                        [14, 30]],
-             'slit2' : [[492, 2305, 1633, 1741],
-                        [12, 14, 50, 40]]}
+        # Checking if the masking task is well done
+        z1l, z1u = np.percentile(dt1, [15, 85])
+        # 2-slit mode
+        if (ic.nslit == 2):
+            z2l, z2u = np.percentile(dt2, [15, 85])
 
-# ------------------ #
+        if (ic.nslit == 1):
+            z1, z2 = z1l, z1u
+            ds9_frm = "ds9 xeqxbrg"+sci0+".fits[2] -multiframe"
+            ds9_loc = " -scale lock yes -frame lock image"
+            ds9_scl = " -scale limits {0:.2f} {1:.2f} &".format(z1, z2)
+        if (ic.nslit == 2):
+            z1, z2 = 0.5*(z1l+z2l), 0.5*(z1u+z2u)
+            ds9_frm = "ds9 xeqxbrg"+sci0+".fits[2] xeqxbrg"+sci0+".fits[5] -multiframe"
+            ds9_loc = " -scale lock yes -frame lock image"
+            ds9_scl = " -scale limits {0:.2f} {1:.2f} &".format(z1, z2)
 
+        os.system(ds9_frm + ds9_loc + ds9_scl)
 
-for i in np.arange(len(iraf.type(ic.lst_sci, Stdout=1))):
-    sci = iraf.type(ic.lst_sci, Stdout=1)[i].strip()
-    exec('dic = copy.deepcopy(fix_sci{0:02d})'.format(i+1))
-    for j in np.arange(ic.nslit):
-    	fmsk = 'mskbadcol_'+sci+'_{0:1d}.txt'.format(j+1)
-    	s = dic['slit{0:1d}'.format(j+1)]
-    	exec('d = dt{0:1d}'.format(j+1))
-    	for k in np.arange(len(s[0])):
-    		if (k == 0):
-    			com = "echo '{0:d} {1:d} 1 {2:d}' > ".format(s[0][k]-s[1][k]/2, s[0][k]+s[1][k]/2, d.shape[0])+fmsk
-    			os.system(com)
-    		else:
-    			com = "echo '{0:d} {1:d} 1 {2:d}' >> ".format(s[0][k]-s[1][k]/2, s[0][k]+s[1][k]/2, d.shape[0])+fmsk
-    			os.system(com)
-    	iraf.text2mask(fmsk, fmsk.strip('.txt')+'.pl', d.shape[1], d.shape[0])
-
-iraf.imdelete('xeqxbrg@'+ic.lst_sci)
-iraf.imdelete('tmp@'+ic.lst_sci)
-iraf.imdelete('tmpdq@'+ic.lst_sci)
-
-for sci in iraf.type(ic.lst_sci, Stdout=1):
-    sci = sci.strip()
-    iraf.copy('eqxbrg'+sci+'.fits', 'tmp'+sci+'.fits')
-    for j in np.arange(ic.nslit):
-    	iraf.proto.fixpix('tmp'+sci+'.fits[sci,{0:1d}]'.format(j+1),
-    		              'mskbadcol_'+sci+'_{0:1d}.pl'.format(j+1),
-    		              linterp='1,2,3,4')
-    iraf.copy('tmp'+sci+'.fits', 'xeqxbrg'+sci+'.fits')
-    for j in np.arange(ic.nslit):
-    	iraf.imarith('mskbadcol_'+sci+'_{0:1d}.pl'.format(j+1), '+',
-    		         'xeqxbrg'+sci+'.fits[dq,{0:1d}]'.format(j+1),
-    		         'tmpdq'+sci+'.fits[dq,{0:1d}]'.format(j+1))
-    	iraf.imcopy('tmpdq'+sci+'.fits[dq,{0:1d}][*,*]'.format(j+1),
-    		        'xeqxbrg'+sci+'.fits[dq,{0:1d}][*,*]'.format(j+1))
-    	iraf.proto.fixpix('tmp'+sci+'.fits[sci,{0:1d}]'.format(j+1),
-    		              'mskbadcol_'+sci+'_{0:1d}.pl'.format(j+1),
-    		              linterp='1,2,3,4')    
-
-
-for sci in iraf.type(ic.lst_sci, Stdout=1):
-	for j in np.arange(ic.nslit):
-		os.system('ds9 &')
-		iraf.sleep(5.0)
-		iraf.display(image = 'eqxbrg'+sci+'.fits[sci,{0:1d}]'.format(j+1), frame=1)
-		iraf.display(image = 'xeqxbrg'+sci+'.fits[sci,{0:1d}]'.format(j+1), frame = 2)
+        # Coming back to current path
+        os.chdir(current_dir)
+        iraf.chdir(current_dir)         
 
 
 # Printing the running time
-print('--- %s seconds ---' %(time.time()-start_time))
+print('--- %.4f seconds ---' %(time.time()-start_time))
