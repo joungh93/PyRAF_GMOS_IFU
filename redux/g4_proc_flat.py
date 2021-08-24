9b35c7ca0066aa4287191136c343c6bdf5c853eb
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-08-24 17:44:39 +0900

    Update g4_proc_flat.py

--- redux/g4_proc_flat.py
+++ redux/g4_proc_flat.py
@@ -32,65 +32,53 @@ iraf.unlearn('gfresponse')
 
 
 # ---------- Reduce the lamp flat ---------- #
+for d in ic.dir_wav:
+    dir_sci = sorted(glob.glob(d+"/*"))
 
-# Model and remove the light
+    for j in np.arange(len(dir_sci)):
 
-iraf.imdelete('brg@'+ic.lst_flat)
+        # Moving each science directory
+        name_sci = dir_sci[j].split("/")[-1]
+        print("Moving path for "+name_sci+"...")
+        os.chdir(current_dir+"/"+dir_sci[j])
+        iraf.chdir(current_dir+"/"+dir_sci[j])
 
-# os.system('ds9 &')
-# iraf.sleep(5.0)
-for flat in iraf.type(ic.lst_flat, Stdout=1):
-    flat = flat.strip()
-    blkmsk = 'newblkmask_S20190228S0015_hdr01'
-    iraf.gfscatsub('rg'+flat, blkmsk, outimage='', prefix='b',
-    	           xorder='3,3,3,3,3,3,3,3,3,3,3,3',
-    	           yorder='3,3,3,3,3,3,3,3,3,3,3,3', 
-                   cross='yes', fl_inter='no')
+        # FLAT
+        flat = np.loadtxt(ic.lst_flat, dtype=str)
+        flat0 = flat.item(0)
 
-# os.system('ds9 &')
-# iraf.sleep(5.0)
-# for flat in iraf.type(ic.lst_flat, Stdout=1):
-#     flat = flat.strip()
-#     for i in np.arange(12):
-#         iraf.imexamine('brg'+flat+'[sci,'+str(i+1)+']', 1)
+        # ARC
+        arc = np.loadtxt(ic.lst_arc, dtype=str)
+        arc0 = arc.item(0)
 
+        # Model and remove the light
+        iraf.imdelete('brg@'+ic.lst_flat)
+        blkmsk = np.loadtxt("blkmask_name.txt", dtype=str)
+        blkmsk0 = blkmsk.item(0)
+        iraf.gfscatsub('rg'+flat0, blkmsk0, outimage='', prefix='b',
+                       xorder='3,3,3,3,3,3,3,3,3,3,3,3',
+                       yorder='3,3,3,3,3,3,3,3,3,3,3,3', 
+                       cross='yes', fl_inter='no')
 
-# QE correction and extract
-iraf.imdelete('eqbrg@'+ic.lst_flat)
+        # QE correction and extract
+        iraf.imdelete('qbrg@'+ic.lst_flat)
+        iraf.imdelete('eqbrg@'+ic.lst_flat)
+        iraf.gfreduce('brg@'+ic.lst_flat, recenter='no', reference='erg'+flat0,
+                      fl_extract='yes', fl_qecorr='yes', qe_refim='erg'+arc0,
+                      fl_addmdf='no', fl_bias='no', fl_over='no', fl_trim='no',
+                      mdffile=ic.nmdf, mdfdir='./',
+                      slits=ic.cslit, line=ic.pk_line, fl_fluxcal='no', fl_gscrrej='no',
+                      fl_wavtran='no', fl_skysub='no', fl_inter='no', fl_vardq='yes')
 
-flatref = iraf.type(ic.lst_flat, Stdout=1)[0]
-arc0 = iraf.type(ic.lst_arc, Stdout=1)[0]
+        # Response function
+        iraf.imdelete(flat0+'_resp')
+        iraf.gfresponse('eqbrg'+flat0, outimage=flat0+'_resp', sky='', 
+                        order=45, func='spline3', sample='*', 
+                        fl_fit='yes', fl_inter='no')
 
-iraf.imdelete('qbrg@'+ic.lst_flat)
-iraf.imdelete('eqbrg@'+ic.lst_flat)
-iraf.gfreduce('brg@'+ic.lst_flat, recenter='no', reference='erg'+flatref,
-              fl_extract='yes', fl_qecorr='yes', qe_refim='erg'+arc0,
-              fl_addmdf='no', fl_bias='no', fl_over='no', fl_trim='no',
-              mdffile=ic.nmdf, mdfdir='./',
-              slits='both', line=1400, fl_fluxcal='no', fl_gscrrej='no',
-              fl_wavtran='no', fl_skysub='no', fl_inter='no',
-              fl_vardq='yes')
-
-os.system('ds9 &')
-iraf.sleep(5.0)
-for flat in iraf.type(ic.lst_flat, Stdout=1):
-	flat = flat.strip()
-	iraf.gfdisplay('eqbrg'+flat, 1)
-
-
-# ---------- Response function ---------- #
-for flat in iraf.type(ic.lst_flat, Stdout=1):
-    flat = flat.strip()
-    iraf.imdelete(flat+'_resp')
-    iraf.gfresponse('eqbrg'+flat, outimage=flat+'_resp', sky='', 
-    	            order=45, func='spline3', sample='*', 
-    	            fl_fit='yes', fl_inter='no')
-
-# os.system('ds9 &')
-# iraf.sleep(5.0)
-for flat in iraf.type(ic.lst_flat, Stdout=1):
-    flat = flat.strip()
-    iraf.gfdisplay(flat+'_resp', 1)
+        # Coming back to current path
+        os.chdir(current_dir)
+        iraf.chdir(current_dir)  
 
 
 # Printing the running time
