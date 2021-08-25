214f0119544465044e9f835dd2b1ca6a9bd0c71a
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-08-25 16:36:16 +0900

    Update rv_w0.py

--- redux/rv_w0.py
+++ redux/rv_w0.py
@@ -34,41 +34,46 @@ iraf.rv()
 iraf.unlearn('rvidlines')
 iraf.rv.observatory = ic.obs_site
 
-# rvFlags = {'coordlist':'skylines.txt', 'ftype':'emission', 'nsum':1, 'threshold':7.,
-#            'maxfeatures':13, 'fwidth':10., 'cradius':10., 'minsep':5.,
-#            'logfile':'rvLog.txt', 'autowrite':'yes'}
+for d in ic.dir_wav:
+    dir_sci = sorted(glob.glob(d+"/*"))
 
-for sci in iraf.type(ic.lst_sci, Stdout=1):
-	sci = sci.strip()
+    for j in np.arange(len(dir_sci)):
 
-	hd0 = fits.getheader('stxeqxbrg'+sci+'.fits', ext=0)
-	hd2 = fits.getheader('stxeqxbrg'+sci+'.fits', ext=2)
+        # Moving each science directory
+        name_sci = dir_sci[j].split("/")[-1]
+        print("Moving path for "+name_sci+"...")
+        os.chdir(current_dir+"/"+dir_sci[j])
+        iraf.chdir(current_dir+"/"+dir_sci[j])
 
-	# fits.setval('stxeqxbrg'+sci+'.fits', 'DATE-OBS', value=hd0['DATE-OBS'], ext=5)
-	# fits.setval('stxeqxbrg'+sci+'.fits', 'UT', value=hd0['UT'], ext=5)
-	# fits.setval('stxeqxbrg'+sci+'.fits', 'RA', value=hd0['RA'], ext=5)
-	# fits.setval('stxeqxbrg'+sci+'.fits', 'DEC', value=hd0['DEC'], ext=5)
+        # SCI
+        sci = np.loadtxt(ic.lst_sci, dtype=str)
+        sci0 = sci.item(0)
 
-	os.system('rm -rfv rvLog_'+sci+'.txt')
+        # Running rvidlines
+        hd0 = fits.getheader('stxeqxbrg'+sci0+'.fits', ext=0)    # Primary HDU
+        hd2 = fits.getheader('stxeqxbrg'+sci0+'.fits', ext=2)    # SCI HDU
 
-	# iraf.rvidlines('stxeqxbrg'+sci+'.fits[SKY]', **rvFlags)
-	iraf.rvidlines('stxeqxbrg'+sci+'.fits[SKY]', coordlist='skylines.txt', ftype='emission',
-                   nsum=1, threshold=7.0, maxfeatures=13, fwidth=10.0, cradius=10.0, minsep=5.0,
-                   autowrite='yes', logfile='rvLog_'+sci+'.txt')
+        os.system('rm -rfv rvLog_'+sci0+'.txt')              
+        iraf.rvidlines('stxeqxbrg'+sci0+'.fits[SKY]', coordlist=ic.dir_iraf+'skylines.txt', ftype='emission',
+                       nsum=1, threshold=7.0, maxfeatures=13, fwidth=10.0,
+                       cradius=10.0, minsep=5.0, autowrite='yes',
+                       logfile='rvLog_'+sci0+'.txt')
 
-	f = open('rvLog_'+sci+'.txt','r')
-	lines = f.readlines()
-	bool_Zobs = pd.Series(lines).str.startswith('stxeqxbrg'+sci+'.fits[SKY]   1 : Zobs').values
-	line_Zobs = np.array(lines)[bool_Zobs][0]
-	Zobs = float(line_Zobs.split('Zobs     =')[1].split(',')[0])
-	f.close()
+        f = open('rvLog_'+sci0+'.txt','r')
+        lines = f.readlines()
+        bool_Zobs = pd.Series(lines).str.startswith('stxeqxbrg'+sci0+'.fits[SKY]   1 : Zobs').values
+        line_Zobs = np.array(lines)[bool_Zobs][0]
+        Zobs = float(line_Zobs.split('Zobs     =')[1].split(',')[0])
+        f.close()
 
-	print('{0:.3f} --> {1:.3f}'.format(hd2['CRVAL1'], hd2['CRVAL1']+(-Zobs*hd0['CENTWAVE']*10.0)))
-	nw_zero = hd2['CRVAL1']+(-Zobs*hd0['CENTWAVE']*10.0)
+        print('{0:.3f} --> {1:.3f}'.format(hd2['CRVAL1'], hd2['CRVAL1']+(-Zobs*hd0['CENTWAVE']*10.0)))
+        nw_zero = hd2['CRVAL1']+(-Zobs*hd0['CENTWAVE']*10.0)
 
-	iraf.unlearn('hedit')
-	iraf.hedit.update = 'yes'
-	iraf.hedit.verify = 'no'
+        iraf.unlearn('hedit')
+        iraf.hedit.update = 'yes'
+        iraf.hedit.verify = 'no'
+        iraf.hedit('stxeqxbrg'+sci0+'.fits[SCI]', 'CRVAL1', nw_zero)
 
-	iraf.hedit('stxeqxbrg'+sci+'.fits[SCI]', 'CRVAL1', nw_zero)
-
+        # Coming back to current path
+        os.chdir(current_dir)
+        iraf.chdir(current_dir)  
