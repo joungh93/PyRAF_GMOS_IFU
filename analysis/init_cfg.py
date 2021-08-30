539e1fb93018bcbcc77dfa9e780c7ee3f2a99475
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-08-30 17:52:09 +0900

    Update init_cfg.py

--- analysis/init_cfg.py
+++ analysis/init_cfg.py
@@ -44,6 +44,6 @@ cube_ref = ''    # Reference cube
 # ----- Wavelength setting ----- #
 redshift = 0.3424    # Redshift of galaxy
 wav_range_res = np.array([6520.0, 6600.0])    # H alpha wavelength range (rest-frame)
-check_x = [15, 55]    # [xmin, xmax] for check
-check_y = [5, 45]    # [ymin, ymax] for check
+check_x = [15, 55]    # [xmin, xmax] for check (including object and some bad regions)
+check_y = [5, 45]    # [ymin, ymax] for check (including object and some bad regions)
 
