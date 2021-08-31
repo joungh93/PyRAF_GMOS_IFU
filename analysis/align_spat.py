e8c4f6cfc30a1afe13bc891b7a6a9a25babf2910
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-08-31 21:35:00 +0900

    Update align_spat.py

--- analysis/align_spat.py
+++ analysis/align_spat.py
@@ -13,77 +13,65 @@ import numpy as np
 import glob, os
 import copy
 import sys
+from astropy.io import fits
+from scipy import ndimage
+from tqdm import trange
 import init_cfg as ic
 
 
-# ----- Importing IRAF from the root directory ----- #
+# ----- Directories ----- #
 current_dir = os.getcwd()
-os.chdir(ic.dir_iraf)
-
-from pyraf import iraf
-
-
-# ----- Loading the processed cubes ----- #
-from astropy.io import fits
-
-dir_cmb = '/data/jlee/DATA/Gemini/Programs/GN-2019A-Q-215/analysis/combine/'
-split_dir = dir_cmb+'split/'
-
-working_dir = dir_cmb
+working_dir = ic.dir_cmb
+split_dir = ic.dir_cmb+"split/"
 os.chdir(working_dir)
-iraf.chdir(working_dir)
-
 os.system('rm -rfv '+split_dir)
 os.system('mkdir '+split_dir)
 
 
 # ----- Making the split 2D images for spatial alignment ----- #
-wav_range = [5000, 9500]    # Total wavelength range
-# check_Ha = [[2159,2170], [2168,2179]]
 
+# Reading wavelength range
+wav_0, wav_1, dwav = [], [], []
 for i in np.arange(len(ic.cube_list)):
-	fits_id = ic.cube_list[i].split('/')[-1].split('cstxeqxbrg')[-1].split('_3D.fits')[0]
+	h_sci = fits.getheader(ic.cube_list[i], ext=1)
+	wav = np.linspace(start=h_sci['CRVAL3']+(1-h_sci['CRPIX3'])*h_sci['CD3_3'],
+                      stop=h_sci['CRVAL3']+(h_sci['NAXIS3']-h_sci['CRPIX3'])*h_sci['CD3_3'],
+                      num=h_sci['NAXIS3'], endpoint=True)
+	wav_0.append(wav[10])
+	wav_1.append(wav[-11])
+	dwav.append(h_sci['CD3_3'])
+wav_0, wav_1, dwav = np.array(wav_0), np.array(wav_1), np.array(dwav)
 
+# Total wavelength range
+wav_range = [10 * (1 + wav_0.max() // 10),
+             10 * (wav_1.min() // 10)]
+nw_cut = int(round((wav_range[1]-wav_range[0])/np.mean(dwav))) + 1 
+
+# Making the split images
+for i in np.arange(len(ic.cube_list)):
 	hd0 = fits.getheader(ic.cube_list[i], ext=0)
 	d_sci, h_sci = fits.getdata(ic.cube_list[i], ext=1, header=True)
 	d_var, h_var = fits.getdata(ic.cube_list[i], ext=2, header=True)
-	# d_dq, h_dq = fits.getdata(ic.cube_list[i], ext=3, header=True)
 
-	wav = np.linspace(start=h_sci['CRVAL3'], stop=h_sci['CRVAL3']+(h_sci['NAXIS3']-1)*h_sci['CD3_3'],
-	                  num=h_sci['NAXIS3'], endpoint=True)
-
-	# # Removing negative values
-	# d_sci[d_sci < 0.0] = 0.0
-
-	# # Removing bad pixel values
-	# if (hd0['CENTWAVE'] == 700.0):
-	# 	count_max = np.max(d_sci[check_Ha[0][0]:check_Ha[0][1], 2:46, 33:65])
-	# if (hd0['CENTWAVE'] == 680.0):
-	# 	count_max = np.max(d_sci[check_Ha[1][0]:check_Ha[1][1], 2:46, 33:65])
-
-	# d_sci[d_sci > count_max] = 0.0
+	wav = np.linspace(start=h_sci['CRVAL3']+(1-h_sci['CRPIX3'])*h_sci['CD3_3'],
+                      stop=h_sci['CRVAL3']+(h_sci['NAXIS3']-h_sci['CRPIX3'])*h_sci['CD3_3'],
+                      num=h_sci['NAXIS3'], endpoint=True)
 
 	# Wavelength cut
-	wav_start, wav_end = wav_range[0], wav_range[1]
-	nw_cut = int(round((wav_end-wav_start)/h_sci['CD3_3']))
-	spx_start = np.abs(wav-wav_start).argmin()
+	spx_start = np.abs(wav-wav_range[0]).argmin()
 	spx_end = spx_start + nw_cut
 
 	d_sci_cut = d_sci[spx_start:spx_end,:,:]
 	d_var_cut = d_var[spx_start:spx_end,:,:]
-	# d_dq_cut = d_dq[spx_start:spx_end,:,:]
 
 	os.chdir(split_dir)
-	print('Working on '+fits_id+'\n')
-	for j in np.arange(np.shape(d_sci_cut)[0]):
-		fits.writeto(fits_id+'_2D_{0:04d}.fits'.format(j+1), d_sci_cut[j,:,:], h_sci, overwrite=True)
-		fits.writeto(fits_id+'_VAR_{0:04d}.fits'.format(j+1), d_var_cut[j,:,:], h_var, overwrite=True)
+	print('Working on '+ic.cube_name[i])
+	for j in trange(np.shape(d_sci_cut)[0]):
+		fits.writeto(ic.cube_name[i]+'_SCI_{0:04d}.fits'.format(j+1), d_sci_cut[j,:,:], h_sci, overwrite=True)
+		fits.writeto(ic.cube_name[i]+'_VAR_{0:04d}.fits'.format(j+1), d_var_cut[j,:,:], h_var, overwrite=True)
 	os.chdir(working_dir)
 
-
-# Stop point #1
-sys.exit('Split images generated')
-
+'''
 
 # Shifting cubes with offset.txt (IRAF/imshift task)
 os.chdir(split_dir)
@@ -158,11 +146,9 @@ for i in np.arange(len(ic.cube_list)):
 	print('Written : al1_'+fits_id+'_3D.fits')		
 
 	os.chdir(working_dir)
-
+'''
 
 os.chdir(current_dir)
-iraf.chdir(current_dir)
-
 
 
 # Printing the running time
