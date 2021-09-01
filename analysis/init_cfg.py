1f14067dbce78efaa3ed272855379c14c2d86d6a
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-09-01 15:17:46 +0900

    Update init_cfg.py

--- analysis/init_cfg.py
+++ analysis/init_cfg.py
@@ -9,6 +9,7 @@ Created on Tue Oct 29 16:53:20 2019
 
 import numpy as np
 import glob, os
+from astropy.io import fits
 
 
 # ----- Directories ----- #
@@ -48,3 +49,20 @@ wav_range_res = np.array([6520.0, 6600.0])    # H alpha wavelength range (rest-f
 check_x = [15, 55]    # [xmin, xmax] for check (including object and some bad regions)
 check_y = [5, 45]    # [ymin, ymax] for check (including object and some bad regions)
 
+# Reading wavelength range
+wav_0, wav_1, dwav = [], [], []
+for i in np.arange(len(cube_list)):
+	h_sci = fits.getheader(cube_list[i], ext=1)
+	wav = np.linspace(start=h_sci['CRVAL3']+(1-h_sci['CRPIX3'])*h_sci['CD3_3'],
+                      stop=h_sci['CRVAL3']+(h_sci['NAXIS3']-h_sci['CRPIX3'])*h_sci['CD3_3'],
+                      num=h_sci['NAXIS3'], endpoint=True)
+	wav_0.append(wav[10])
+	wav_1.append(wav[-11])
+	dwav.append(h_sci['CD3_3'])
+wav_0, wav_1, dwav = np.array(wav_0), np.array(wav_1), np.array(dwav)
+
+# Total wavelength range
+wav_range = [10 * (1 + wav_0.max() // 10),
+             10 * (wav_1.min() // 10)]
+nw_cut = int(round((wav_range[1]-wav_range[0])/np.mean(dwav))) + 1 
+wav_intv = 1.0    # the resulting wavelength interval
