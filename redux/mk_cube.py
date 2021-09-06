f5695773f1b01cd352db842614efb472432240e7
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-09-06 18:08:03 +0900

    Update mk_cube.py

--- redux/mk_cube.py
+++ redux/mk_cube.py
@@ -52,7 +52,7 @@ for d in ic.dir_wav:
                     fl_atmdisp='yes', fl_var='yes', fl_dq='yes')
 
         fits.open('cstxeqxbrg'+sci0+'_3D.fits').info()
-        os.system('ds9 cstxeqxbrg'+sci0+'_3D.fits[sci] &')
+        os.system('ds9 -scalemode zscale cstxeqxbrg'+sci0+'_3D.fits[sci] &')
 
         # Coming back to current path
         os.chdir(current_dir)
