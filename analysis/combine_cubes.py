389930b134f6b22800ad1c593e11d9a3efaf8197
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-10-07 21:59:51 +0900

    Update combine_cubes.py

--- analysis/combine_cubes.py
+++ analysis/combine_cubes.py
@@ -35,7 +35,10 @@ f_var = np.zeros((d_sci.shape[0], d_sci.shape[1], d_sci.shape[2]))
 for k in tqdm.trange(d_sci.shape[0]):
 	ds = np.zeros((len(al2_cubes), d_sci.shape[1], d_sci.shape[2]))
 	dv = np.zeros((len(al2_cubes), d_sci.shape[1], d_sci.shape[2]))
+	if (len(ic.cube_spa_off) > 0):
+		ds2 = np.zeros((len(al2_cubes)-len(ic.cube_spa_off), d_sci.shape[1], d_sci.shape[2]))
 
+	j = 0
 	for i in np.arange(len(al2_cubes)):
 		hd0 = fits.getheader(al2_cubes[i], ext=0)
 		d_sci, h_sci = fits.getdata(al2_cubes[i], ext=1, header=True)
@@ -47,13 +50,26 @@ for k in tqdm.trange(d_sci.shape[0]):
 
 		ds[i,:,:] = d_sci[k,:,:]
 		dv[i,:,:] = d_var[k,:,:]
-		
+
+		if (al2_cubes[i].split("al2_")[-1].split("_3D.fits")[0] not in ic.cube_spa_off):
+			ds2[j,:,:] = d_sci[k,:,:]
+			j += 1
+
 	if (ic.combine_mode == 'median'):
 		cds = np.nanmedian(ds, axis=0)
 	if (ic.combine_mode == 'clippedmean'):
 		clipped_ds = sigma_clip(ds, sigma=3.0, axis=0).data
 		cds = np.nanmean(clipped_ds, axis=0)
 	cdv = np.sum(dv, axis=0) / ds.shape[0]**2.0
+
+	##### Needed to be revised manually after visual check! #####
+	if ((len(ic.cube_spa_off) > 0) & (ic.overwrite == True)):
+		if (ic.combine_mode == 'median'):
+			cds[:,61:] = np.median(ds2[:,:,61:], axis=0)
+		if (ic.combine_mode == 'clippedmean'):
+			clipped_ds2 = sigma_clip(ds2, sigma=3.0, axis=0).data
+			cds[:,61:] = np.nanmean(clipped_ds2[:,:,61:], axis=0)
+	##########
 
 	f_sci[k,:,:] = cds
 	f_var[k,:,:] = cdv
