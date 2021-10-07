dd14c33fe56712b04ef039677ededa6ffca004ce
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2021-10-07 21:58:13 +0900

    Update check_offset.py

--- analysis/check_offset.py
+++ analysis/check_offset.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 20 15:44:25 2020
-
+Created on Thu Feb 27 15:18:20 2020
 @author: jlee
 """
 
@@ -11,116 +10,150 @@ start_time = time.time()
 
 import numpy as np
 import glob, os
-import init_cfg as ic
+import copy
+import sys
 from astropy.io import fits
+from astropy.convolution import Gaussian2DKernel
+from astropy.convolution import convolve
 from scipy import ndimage
+from tqdm import trange
+import init_cfg as ic
+import warnings
+warnings.filterwarnings("error")
 
 
-# ----- Loading Ha sum images ----- #
+# ----- Directories ----- #
 current_dir = os.getcwd()
 working_dir = ic.dir_cmb
+split_dir = ic.dir_cmb+"split/"
 os.chdir(working_dir)
-# Ha_list = sorted(glob.glob("Ha_sum-*.fits"))
+os.system('rm -rfv '+split_dir)
+os.system('mkdir '+split_dir)
 
 
-# ----- Calculating WCS offsets ----- #
+# ----- Making the split 2D images for spatial alignment ----- #
+kernel = Gaussian2DKernel(2)
+p = np.genfromtxt(current_dir+"/count_max.txt", dtype=None, encoding=None, 
+	              names=("cb","val"))
+count_max = p['val'].max()
 
-# WCS reference
-idx_ref = np.where([ic.cube_ref in cb for cb in ic.cube_list])[0][0]
-h01 = fits.getheader(ic.cube_list[idx_ref], ext=0)
-PA = h01['PA']*np.pi/180.
-
-# Writing WCS offset file
-f = open('offset.txt', 'w')
+# Making the split images
 for i in np.arange(len(ic.cube_list)):
-	h = fits.getheader(ic.cube_list[i], ext=0)
+	hd0 = fits.getheader(ic.cube_list[i], ext=0)
+	d_sci, h_sci = fits.getdata(ic.cube_list[i], ext=1, header=True)
+	d_var, h_var = fits.getdata(ic.cube_list[i], ext=2, header=True)
 
-	offset_RA = (h['CRVAL1']-h01['CRVAL1'])*np.cos(h01['CRVAL2'])*3600.0    # arcsec
-	offset_Dec = (h['CRVAL2']-h01['CRVAL2'])*3600.0    # arcsec
+	wav = np.linspace(start=h_sci['CRVAL3']+(1-h_sci['CRPIX3'])*h_sci['CD3_3'],
+                      stop=h_sci['CRVAL3']+(h_sci['NAXIS3']-h_sci['CRPIX3'])*h_sci['CD3_3'],
+                      num=h_sci['NAXIS3'], endpoint=True)
 
-	offset_X = (+offset_RA*np.cos(PA) - offset_Dec*np.sin(PA)) / ic.pixel_scale    # pixel
-	offset_Y = (+offset_RA*np.sin(PA) + offset_Dec*np.cos(PA)) / ic.pixel_scale    # pixel
+	# Wavelength cut
+	spx_start = np.abs(wav-ic.wav_range[0]).argmin()
+	spx_end = spx_start + ic.nw_cut
 
-	print(ic.cube_name[i]+f" - RA offset: {offset_RA:.3f}, Dec offset: {offset_Dec:.3f}")
-	print(f"                 X offset: {offset_X:.3f} pix, Y offset: {offset_Y:.3f}")
-	print(f"                 Reference pixel ({h['CRPIX1']:.3f}, {h['CRPIX2']:.3f}) \n")
-	f.write(f"{offset_X:.3f}  {offset_Y:.3f} \n")
-f.close()
+	d_sci_cut = d_sci[spx_start:spx_end,:,:]
+	d_var_cut = d_var[spx_start:spx_end,:,:]
+
+	assert (p['cb'][i] == ic.cube_name[i])
+
+	os.chdir(split_dir)
+	print('Working on '+ic.cube_name[i])
+	for j in trange(np.shape(d_sci_cut)[0]):
+		d_sci_cut2 = d_sci_cut[j,:,:]
+		d_var_cut2 = d_var_cut[j,:,:]
+
+		d_sci_cut2[d_sci_cut2 > count_max] = np.nan
+		d_sci_cut2[d_sci_cut2 < -count_max] = 0.0
+
+		try:
+			conv = convolve(d_sci_cut2, kernel)
+			d_sci_cut2[np.isnan(d_sci_cut2)] = conv[np.isnan(d_sci_cut2)]
+		except:
+			try:
+				conv2 = convolve(d_sci_cut2, Gaussian2DKernel(4))
+				d_sci_cut2[np.isnan(d_sci_cut2)] = conv2[np.isnan(d_sci_cut2)]
+			except:
+				d_sci_cut2[np.isnan(d_sci_cut2)] = 0.5*(count_max + np.nanmedian(d_sci_cut2))
+
+		fits.writeto(ic.cube_name[i]+f"_SCI_{j+1:04d}.fits", d_sci_cut2, h_sci, overwrite=True)
+		fits.writeto(ic.cube_name[i]+f"_VAR_{j+1:04d}.fits", d_var_cut2, h_var, overwrite=True)
+	os.chdir(working_dir)
+print('----- Split images were created. -----\n')
 
 
-# ----- Shifting & combining Ha sum images ----- #
+# ----- Shifting cubes with offset.txt ----- #
+os.chdir(split_dir)
+offset_X, offset_Y = np.loadtxt(ic.dir_cmb+"offset.txt").T
+for j in trange(d_sci_cut.shape[0]):
+	for k in np.arange(len(ic.cube_name)):
+		ds, hs = fits.getdata(ic.cube_name[k]+f"_SCI_{j+1:04d}.fits", header=True)
+		ds_shifted = ndimage.shift(ds, shift=(-offset_Y[k], -offset_X[k]), mode='nearest')
+		fits.writeto("al1_"+ic.cube_name[k]+f"_SCI_{j+1:04d}.fits", ds_shifted, hs, overwrite=True)
 
-# Running an initial shift task
-offset_X, offset_Y = np.loadtxt('offset.txt').T
-dhs0 = fits.getdata(sorted(glob.glob("Ha_sum-*.fits"))[0], ext=0, header=False)
-dhs2 = np.zeros((len(ic.cube_list), dhs0.shape[0], dhs0.shape[1]))
-if (len(ic.cube_spa_off) > 0):
-	dhs3 = np.zeros((len(ic.cube_list)-len(ic.cube_spa_off), dhs0.shape[0], dhs0.shape[1]))
-j = 0
+		dv, hv = fits.getdata(ic.cube_name[k]+f"_VAR_{j+1:04d}.fits", header=True)
+		dv_shifted = ndimage.shift(dv, shift=(-offset_Y[k], -offset_X[k]), mode='nearest')
+		fits.writeto("al1_"+ic.cube_name[k]+f"_VAR_{j+1:04d}.fits", dv_shifted, hv, overwrite=True)
+
+os.chdir(working_dir)	
+print('----- Shift images were created. -----\n')
+
+
+# ----- Saving new cubes ([SCI, VAR]) ----- #
+os.system("rm -rfv "+working_dir+"cube1")
+os.system("mkdir "+working_dir+"cube1")
+
 for i in np.arange(len(ic.cube_list)):
-	dhs, hdr = fits.getdata("Ha_sum-"+ic.cube_name[i]+".fits", ext=0, header=True)
-	dhs_shifted = ndimage.shift(dhs, shift=(-offset_Y[i], -offset_X[i]), mode='nearest')
-	fits.writeto('al1_Ha_sum-'+ic.cube_name[i]+'.fits', dhs_shifted, hdr, overwrite=True)
-	dhs2[i,:,:] = dhs_shifted
-	if (ic.cube_name[i] not in ic.cube_spa_off):
-		dhs3[j,:,:] = dhs_shifted
-		j += 1
+	hd0 = fits.getheader(ic.cube_list[i], ext=0)
+	d_sci, h_sci = fits.getdata(ic.cube_list[i], ext=1, header=True)
+	d_var, h_var = fits.getdata(ic.cube_list[i], ext=2, header=True)
 
-# Combining the shifted images
-cdhs = np.median(dhs2, axis=0)
+	# assert (p['cb'][i] == ic.cube_name[i])
+	# count_max = p['val'].max()
+	# d_sci[d_sci > count_max] = 0.0
+	
+	wav = np.linspace(start=h_sci['CRVAL3']+(1-h_sci['CRPIX3'])*h_sci['CD3_3'],
+                      stop=h_sci['CRVAL3']+(h_sci['NAXIS3']-h_sci['CRPIX3'])*h_sci['CD3_3'],
+                      num=h_sci['NAXIS3'], endpoint=True)
 
-##### Needed to be revised manually after visual check! #####
-if ((len(ic.cube_spa_off) > 0) & (ic.overwrite == True)):
-	cdhs[:,61:] = np.median(dhs3[:,:,61:], axis=0)
-##########
+	spx_start = np.abs(wav-ic.wav_range[0]).argmin()
+	spx_end = spx_start + ic.nw_cut
 
-fits.writeto('al1_fcomb.fits', cdhs, overwrite=True)
-disp = "ds9 -scalemode zscale -scale lock yes -frame lock image "
-tile = "-tile grid mode manual -tile grid layout "
-cross = "-mode crosshair -lock crosshair image "
-os.system(disp+cross+"Ha_sum-*.fits al1_Ha_sum-*.fits "+tile+f"{len(ic.cube_list):d} 2 &")
-os.system(disp+"al1_fcomb.fits &")
+	d_sci_cut = d_sci[spx_start:spx_end,:,:]
+	d_var_cut = d_var[spx_start:spx_end,:,:]
 
+	al1_d_sci = np.zeros((np.shape(d_sci_cut)[0], np.shape(d_sci_cut)[1], np.shape(d_sci_cut)[2]))
+	al1_d_var = np.zeros((np.shape(d_sci_cut)[0], np.shape(d_sci_cut)[1], np.shape(d_sci_cut)[2]))
 
-# # ----- Running IRAF/xregister task for the shifted Ha sum images ----- #
-# iraf.images()
-# iraf.immatch()
-# iraf.unlearn('xregister')
+	for j in np.arange(np.shape(d_sci_cut)[0]):
+		d_sci2 = fits.getdata(split_dir+"al1_"+ic.cube_name[i]+f"_SCI_{j+1:04d}.fits")
+		al1_d_sci[j,:,:] = d_sci2
+		d_var2 = fits.getdata(split_dir+"al1_"+ic.cube_name[i]+f"_VAR_{j+1:04d}.fits")
+		al1_d_var[j,:,:] = d_var2
 
-# os.system('ls -1 al1_Ha_sum-*.fits > input_al1.lis')
-# os.system('rm -rfv shifts.db rgal1_Ha_sum-*.fits')
-# iraf.xregister(input='@input_al1.lis', reference='al1_Ha_sum-N20190613S0237.fits', regions='[42:62, 10:36]',
-#                shifts='shifts.db', output='rg//@input_al1.lis', databasefmt='no', correlation='fourier',
-#                interp_type='linear', boundary_type='nearest')
+	os.chdir(working_dir+"cube1")
 
-# rgal1_list = sorted(glob.glob('rgal1_Ha_sum*.fits'))
+	nhd0 = fits.PrimaryHDU()
+	nhd1 = fits.ImageHDU()
+	nhd2 = fits.ImageHDU()
 
-# f1 = open('rgal1_comb1.lis', 'w')
-# f2 = open('rgal1_comb2.lis', 'w')
-# for i in np.arange(len(ic.cube_list)):
-# 	cb = ic.cube_list[i].split('/')[-1].split('cstxeqxbrg')[-1].split('_3D.fits')[0]
-# 	# cb = ic.cube_list[i].split('_3D.fits')[0].split(ic.dir_redux+'cstxeqxbrg')[1]
-# 	if (cb not in ic.cube_spa_off):
-# 		f1.write(rgal1_list[i]+'\n')
-# 	else:
-# 		f2.write(rgal1_list[i]+'\n')
-# f1.close()
-# f2.close()
+	nhd0.header = hd0
 
-# # Combining w/ IRAF/imcombine task
-# outimg = ['rgal1_Ha_comb1.fits', 'rgal1_Ha_comb2.fits']
-# for i in np.arange(len(outimg)):
-# 	os.system('rm -rfv '+outimg[i])
-# 	iraf.imcombine(input='@rgal1_comb{0:1d}.lis'.format(i+1), output=outimg[i], combine='median')
-# 	exec('dat_rgal1_comb{0:1d} = fits.getdata(outimg[i])'.format(i+1))
+	nhd1.data = al1_d_sci
+	nhd1.header = h_sci
+	nhd1.header['CRPIX3'] = 1.
+	nhd1.header['CRVAL3'] = wav[spx_start]
 
-# dat_rgal1_fcomb = 0.5*(dat_rgal1_comb1+dat_rgal1_comb2)
-# dat_rgal1_fcomb[:, 62-1:] = dat_rgal1_comb1[:,62-1:]
+	nhd2.data = al1_d_var
+	nhd2.header = h_var
+	nhd2.header['CRPIX3'] = 1.
+	nhd2.header['CRVAL3'] = wav[spx_start]
 
-# fits.writeto('rgal1_fcomb.fits', dat_rgal1_fcomb, overwrite=True)
+	cb_hdu = fits.HDUList([nhd0, nhd1, nhd2])
+	cb_hdu.writeto("al1_"+ic.cube_name[i]+"_3D.fits", overwrite=True)
 
-# os.system('ds9 -scalemode zscale -scale lock yes -frame lock image rgal1_fcomb.fits &')
+	print("Written: al1_"+ic.cube_name[i]+"_3D.fits")		
 
+	os.chdir(working_dir)
 
 os.chdir(current_dir)
 
